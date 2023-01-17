ca4d0ee3d0d7078341b1b54d8e70ff5c6921d502
Oleg0110 <oleg.buno13@gmail.com>
2023-01-17 15:18:12 +0200

    Connected npm Material UI and created routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import useRoutes from 'utils/routes';
 import styles from './App.module.scss';
 
 const App: React.FC = () => {
-  return <div className={styles.App}></div>;
+  const routes = useRoutes();
+
+  return <div className={styles.App}>{routes}</div>;
 };
 
 export default App;
---
+++ front/src/utils/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Article, Home } from '../layout';
+import { ROUTES } from './constants';
+
+const useRoutes = () => (
+  <Routes>
+    <Route path={ROUTES.home} element={<Home />}></Route>
+    <Route path={`${ROUTES.article}/*`} element={<Article />}></Route>
+    <Route path="*" element={<Navigate to={ROUTES.home} replace />} />
+  </Routes>
+);
+
+export default useRoutes;
